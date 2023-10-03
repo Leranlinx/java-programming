3205e47d17a1e03562d50ca8c8785439abf6e281
Nahimul Islam <nahimul.dti@gmail.com>
2023-10-04 01:29:21 +0600

    Addition for loop

--- Additon_for_lop.java
+++ Additon_for_lop.java
@@ -3,12 +3,12 @@ public class Additon_for_lop {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
 
-        int sum,i,n;
-        n = input.nextInt();
-        sum = 0 ;
-        for (i=0; i<=n;i++){
-            sum= sum + i ;
+        int n = input.nextInt();
+        int sum = 0;
+
+        for (int i =0; i<=n ; i++ ) {
+            sum = sum+i ;
         }
-        System.out.println("ANS IS : "+sum);
+        System.out.println("result : "+sum);
     }
 }
