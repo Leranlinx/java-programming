97dc49fb039333a3d4b4479ffeaf5062ad20712e
Nahimul Islam <nahimul.dti@gmail.com>
2023-10-13 16:57:45 +0600

    Array.

--- Array in java/Array_user_input.java
+++ Array in java/Array_user_input.java
@@ -2,14 +2,28 @@ import  java.util.Scanner;
 public class Array_user_input {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
-        //optimize-1
+
+        //array print all element 
         int [] arr =  {1,2,3,4,5};
         for (int i=0 ; i<arr.length ; i++) { 
             System.out.println("Array Elements "+i+" = "+arr[i]);
         }
-        //optimize-2
+
+        //difference type array all elenent print 
         for(int element:arr){
             System.out.println(element);
         }
-    }
+
+        // new array declear 
+        int[] array = new int[10];
+        for (int i= 0 ; i<array.length ; i++) {
+            System.out.print("Enter yout array number  : ");
+            System.out.println(array[i]);
+        }
+        array[5]= 100;
+        System.out.println(array[5]);
+
+        //array user input 
+        
+    }   
 }
