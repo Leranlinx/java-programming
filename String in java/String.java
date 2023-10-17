52a4348c354568eee51b6028ef851840ec489ea6
Nahimul Islam <nahimul.dti@gmail.com>
2023-10-17 23:17:33 +0600

    Array string

---
+++ String in java/String.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class String {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        // final String firstname = input.nextLine();
+    }
+}
