24f0ce2b9c4fb4f12ee77ca39af6b9fee2bbd823
Nahimul Islam <nahimul.dti@gmail.com>
2023-10-20 14:39:45 +0600

    Class object method  parametrized constrature.

---
+++ oop in java/Class_5.java
@@ -0,0 +1,40 @@
+//This code use default Constructor 
+//This code use default Constructor 
+//This code use default Constructor 
+
+public class Class_5 {
+    String name ;
+    double id ;
+    String defartment;
+    // Stirng email;
+
+    //use basic prametrized Constructor 
+    //use basic Constructor 
+    //use basic Constructor 
+    Class_5(String name, int id , String def){
+        name = name;
+        id=id ;
+        defartment= def;
+        // email= email;
+    }
+
+    //Basic Method 
+    //Basic Method 
+    //Basic Method 
+    void get_information(){
+        System.out.println("Name = "+name);
+        System.out.println("Id = "+id);
+        System.out.println("Defartment = "+defartment);
+    }
+
+    //Basic prametrized method 
+    //Basic prametrized method 
+    //Basic prametrized method 
+    void set_information(String name, int id , String def){
+        name = name;
+        id=id ;
+        defartment= def;
+    }
+}
+
+
---
+++ oop in java/test_class_5.java
@@ -0,0 +1,7 @@
+public class test_class_5 {
+    public static void main(String[] args) {
+        Class_5 student_1= new Class_5("Md Nahimul Islam", 664187, "Computer Scince & Technology");
+        student_1.set_information("Md Nahimul Islam Rafi", 00000, "CSE");
+        student_1.get_information();
+    }
+}
