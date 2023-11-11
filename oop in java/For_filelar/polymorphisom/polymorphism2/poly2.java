7488356f70adb3d112de91b249a89df65c4bbaa9
Nahimul Islam <nahimul.dti@gmail.com>
2023-11-12 03:36:55 +0600

    OOP  Polymorphism.

---
+++ oop in java/For_filelar/polymorphisom/polymorphism2/poly2.java
@@ -0,0 +1,9 @@
+package For_filelar.polymorphisom.polymorphism2;
+
+/**
+ * poly2
+ */
+public class poly2 {
+
+    
+}
---
+++ oop in java/For_filelar/polymorphisom/rectangle.java
@@ -0,0 +1,18 @@
+package For_filelar.polymorphisom;
+
+public class rectangle extends shape {
+    void drow (){
+        System.out.println("1. Drowing a recdtangle");
+        System.out.println("Your rectangle is drowing complet");
+    }
+    // public static void main(String[] args) {
+    //     rectangle r1 = new rectangle();
+    //     r1.drow();
+
+    //     shape s1 = new shape();
+    //     s1.drow();
+
+    //     ciracle c1 = new shape();
+    //     c1.drow();
+    }
+
